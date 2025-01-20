b1d2ac2fff298c4bcce586f0d4a21b05600295cc
T0adSage45 <okami8542@gmail.com>
2025-01-20 21:02:32 +0900

    working on nvf

--- modules/home/nvf.nix
+++ modules/home/nvf.nix
@@ -2,12 +2,15 @@
 {
   programs.nvf = {
         enable = true;
-        settings = {
             vim.theme.enable = true;
             vim.theme.name = "gruvbox";
             vim.theme.style = "dark";
 
             vim.languages.nix.enable = true;
-        };
+            statusline.lualine.enable = true;
+            telescope.enable = true;
+            vim.lsp.enable = true;
+            autocomplete.nvim.cmp.enable = true;
+
     };
 }
