d259221ebee189a94d437d2ab7a16fa251836488
T0adSage45 <okami8542@gmail.com>
2025-01-23 17:06:29 +0900

    little changes

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -2,6 +2,15 @@
 {
   programs.alacritty = {
     enable = true;
-
+    settings = {
+      window.padding = {
+        x = 10;
+        y = 10;
+      };
+      window.decorations = "none";
+      font = {
+        size = 10;
+      };
+    };
   };
 }
