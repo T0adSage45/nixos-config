e21567608c539a9dce1f197c7ccaf4fc68742aba
T0adSage45 <okami8542@gmail.com>
2025-02-20 18:42:50 +0900

    some colors in life ...

--- modules/home/themes/nix-color.nix
+++ modules/home/themes/nix-color.nix
@@ -15,11 +15,11 @@
   # colorScheme = inputs.nix-colors.colorSchemes.twilight;
   # colorScheme = inputs.nix-colors.colorSchemes.tokyo-city-dark;
   # colorScheme = inputs.nix-colors.colorSchemes.tender;
-  #colorScheme = inputs.nix-colors.colorSchemes.tarot; ---
+ # colorScheme = inputs.nix-colors.colorSchemes.tarot;
   # colorScheme = inputs.nix-colors.colorSchemes.tango;
   # colorScheme = inputs.nix-colors.colorSchemes.summerfruit-dark;
-  # colorScheme = inputs.nix-colors.colorSchemes.atelier-forest;
-  colorScheme = inputs.nix-colors.colorSchemes.blueforest;
+   colorScheme = inputs.nix-colors.colorSchemes.atelier-forest;
+  # colorScheme = inputs.nix-colors.colorSchemes.blueforest;
   # colorScheme = inputs.nix-colors.gruvbox-medium-dark;
 
 }
