bff8633a136deb5dbfd9594882b83122da164cbb
T0adSage45 <okami8542@gmail.com>
2025-05-23 01:07:34 +0900

    some configuration changes ...

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -5,7 +5,7 @@
     # BACKGROUND
     background {
       monitor =
-      path = ${../../../Wallpapers/bridge.jpg}
+      path = ${../../../Wallpapers/1.jpeg}
       blur_passes = 2
       contrast = 0.8916
       brightness = 0.8172
